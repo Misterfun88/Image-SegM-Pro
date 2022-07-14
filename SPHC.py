46b2f2a6cd25f10ebec951a25635fbddb5af00c2
Misterfun88 <misterfu7@proton.me>
2022-07-14 15:57:34 +0000

    remove debugging message

---
+++ SPHC.py
@@ -0,0 +1,9 @@
+
+### Superpixel Hierarchical Clustering algorithm (SPHC)
+## Method and Code for merging Superpixels created by Paul Thompson (Please credit if you use this code)
+# The algorithm takes image superpixels created by the skimage SLIC function and merges neighors in order of
+# color similarity (using euclidean distance).
+
+#References:
+#1. http://www.kev-smith.com/papers/SLIC_Superpixels.pdf
+#2. http://www.pyimagesearch.com/2014/07/28/a-slic-superpixel-tutorial-using-python/
